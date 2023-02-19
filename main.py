64019cc720d473bb6d1e60ef42046892a5a49261
Siarhei <kelinsobw@gmail.com>
2023-02-19 12:37:42 +0300

    поиск доски работает

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
+import image
 from time import sleep
-
+from checkers.game import Game
 import pyautogui
+from PIL import Image, ImageGrab
 
 
-def search_chekers(image):
-    print(image)
+def read_board(position):
+    # board pixel = 384*384
+    # cell = 48*48
+    cells =
+
+def search_chekers(screen):
+    lighthouse = Image.open("lighthouse.png")
+    lighthouse = lighthouse.convert('L')
+    screen = screen.convert('L')
+    width_screen = screen.size[0]
+    heighth_screen = screen.size[1]
+    width_lighthouse = lighthouse.size[0]
+    heighth_lighthouse = lighthouse.size[1]
+    pix_screen = screen.load()
+    pix_lighthouse = lighthouse.load()
+    print(pix_lighthouse[5, 5])
+    print(pix_screen[5, 5])
+    for y in range(0, heighth_screen):
+        for x in range(0,width_screen):
+            if pix_screen[x,y] == pix_lighthouse[0,0]:
+                t = screen.crop((x, y, x+width_lighthouse, y+heighth_lighthouse))
+                if lighthouse == screen.crop((x, y, x+width_lighthouse, y+heighth_lighthouse)):
+                    return (x, y+heighth_lighthouse)
+
 
 def screenshot():
-    myScreenshot = pyautogui.screenshot()
-    search_chekers(myScreenshot)
-    #myScreenshot.save(r'file name.png')
+    my_screeen =  pyautogui.screenshot()
+    position = search_chekers(my_screeen)
+    print(position)
+
 
 
 if __name__=="__main__":
-    while True:
-        sleep(2)
-        screenshot()
+    screenshot()
 
